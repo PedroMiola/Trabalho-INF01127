896c5dcbfb5e2c7a4b0c43fd9b1c6a83efe9d62b
Cauê Scotti <caue.scotti@gmail.com>
2024-02-07 00:13:50 -0300

    Css da página pronto

--- src/pages/UserEdit/index.tsx
+++ src/pages/UserEdit/index.tsx
@@ -2,27 +2,65 @@ import React, { SetStateAction, Dispatch } from 'react'
 import { ButtonGroup } from 'components/ButtonGroup'
 import './UserEdit.css'
 import { MainContainer } from 'components/MainContainer'
+import { Button } from 'components/Button'
+import logo from './../../assets/images/alokamon_A.png'
+import foto_perfil from './../../assets/images/sem_foto.png'
 
 
 type UserEditPageProps = {
     setPage: Dispatch<SetStateAction<string>>
 }
 
-const ContainerAzul = (props: UserEditPageProps) => {
+const ContainerAzul = ({ setPage }: UserEditPageProps) => {
 
     return(
-        <ButtonGroup
-            Button1OnClick={() => props.setPage('Carrosel')}
-            Button1Text="Carrosel"
-            Button2OnClick={() => props.setPage('Login')}
-            Button2Text="Login"
-            Button3OnClick={() => props.setPage('Login')}
-            Button3Text="Login"
-            Button4OnClick={() => props.setPage('Login')}
-            Button4Text="Login"
-            Button5OnClick={() => props.setPage('Login')}
-            Button5Text="Login"
-        />
+        <>
+            <div className='logo'>
+                <img className='imgLogo' src={logo}/>
+            </div>
+            <ButtonGroup
+                Button1OnClick={() => setPage('User')}
+                Button1Text="Voltar para Home"
+                Button2OnClick={() => setPage('Login')}
+                Button2Text="Faça um aluguel"
+                Button3OnClick={() => setPage('UserEdit')}
+                Button3Text="Editar cadastro"
+                Button4OnClick={() => setPage('Login')}
+                Button4Text="Seja VIP!"
+                Button5OnClick={() => setPage('NoLogin')}
+                Button5Text="Sair"
+            />
+        </>
+    )
+}
+
+const ContainerBranco = ({ setPage }: UserEditPageProps) => {
+
+    return(
+        <>
+            <div className='mainContainer'>
+                <div className='perfilContainer'>
+                    <img className='profilePicture' src={foto_perfil}/>
+                </div>
+                <div className='formsContainer'>
+                    <div className='topContainer'>
+                        Atualizar cadastro
+                    </div>
+                    <div className='descriptionContainer'>E-mail</div>
+                    <input type="text"/>
+
+                    <div className='descriptionContainer'>Nome</div>
+                    <input type="text"/>
+
+                    <div className='descriptionContainer'>Senha</div>
+                    <input type="text"/>
+
+                    <div className='buttonStyle'>
+                        <Button onClick={() => console.log("Atualizou")} ButtonText='Atualizar'/>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
@@ -32,10 +70,10 @@ export const UserEdit = (props: UserEditPageProps) => {
         <>
             <MainContainer
                 Component1={
-                    <ContainerAzul setPage={props}/>
+                    <ContainerAzul setPage={props.setPage}/>
                 }
                 Component2={
-                    <div>Oi</div>
+                    <ContainerBranco setPage={props.setPage}/>
                 }/>
         </>
     )
